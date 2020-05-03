9624eb9d0a6daaacbdc35b48e88b0ba1388a34af
Valdimiro francisco <valdimiroeinstein@gmail.com>
2020-05-03 04:12:32 +0200

    Inicio do LoginPage

--- app/src/main/java/com/example/longa/HomePage.java
+++ app/src/main/java/com/example/longa/HomePage.java
@@ -1,9 +1,11 @@
 package com.example.longa;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -16,4 +18,6 @@ public class HomePage extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.home,container,false);
     }
+
+
 }
--- app/src/main/java/com/example/longa/MainActivity.java
+++ app/src/main/java/com/example/longa/MainActivity.java
@@ -4,8 +4,11 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -23,7 +26,10 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView bottomNavigationView = findViewById(R.id.neg_bar);
         bottomNavigationView.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
 
+
     }
+
+
 
     private BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =
             new BottomNavigationView.OnNavigationItemSelectedListener() {
